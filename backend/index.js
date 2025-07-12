f9ffe1da24c501b35078b20476566a60eb6dae5c
pandarudra <rudrapanda8206@gmail.com>
2025-07-12 15:45:22 +0530

    fe be added

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+
+const PORT = process.env.PORT || 3000;
+
+async function init() {
+  const app = express();
+  const server = createServer(app);
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+init();
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
